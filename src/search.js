6b8030fd889960c8fa06cc5af4fbb32598512ebf
ZJP <695101044@qq.com>
2019-07-02 10:57:47 +0800

    url-loader

--- src/search.js
+++ src/search.js
@@ -1,1 +1,19 @@
-document.write('search');
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import logo from './img/logo_pc.png'
+import './search.less'
+
+class Search extends React.Component{
+  render() {
+    return <div>search text
+      <img src={logo} />
+    </div>;
+  }
+}
+
+ReactDOM.render(
+  <Search />,
+  document.getElementById('root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -12,4 +12,42 @@ module.exports = {
     path: path.join(__dirname, 'dist'),
     filename: '[name].js'
   },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader'
+        ]
+      },
+      {
+        test: /.(png|jpg|jpeg|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10240
+            }
+          }
+        ]
+      },
+      {
+        test: /.(woff|woff2|eot|ttf|otf)$/,
+        use: 'url-loader'
+      }
+    ]
+  }
 }
